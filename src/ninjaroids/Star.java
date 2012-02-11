c8abd7eececb4c9a548c58543c44bcb5bb5dccf3
Heidi <therandomnarwhal@gmail.com>
2012-02-11 10:46:21 -0800

    2-11-2012~

--- src/ninjaroids/Star.java
+++ src/ninjaroids/Star.java
@@ -1,9 +1,21 @@
 package ninjaroids;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+
 public class Star extends Body
 {
     public Star(int starXarg, int starYarg, int starCourse, int starSpeed)
     {
         super(starXarg, starYarg, starCourse, starSpeed);
+    }
+
+    @Override
+    public void paintSelf(Graphics2D g2)
+    {
+        g2.setColor(Color.green);
+        g2.drawRect(0, 0, 50, 50);
+        g2.drawImage(null, bodyTransform, null);
+        g2.setTransform(bodyTransform);
     }
 }
